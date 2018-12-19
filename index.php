0ecdb6951169e347f46ee64ba218ee948ed142d3
PFlorians <patrikflorians@gmail.com>
2018-12-19 10:40:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html class="container-fluid" lang="en">
+  <head>
+    <!-- meta content -->
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- JS dependencies like jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <!-- axios -->
+			<script src="https://github.com/axios/axios/tree/master/dist/axios.js"></script>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- follow device width initial zoom level -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>demo interface</title>
+  </head>
+  <body>
+
+  </body>
+</html>
